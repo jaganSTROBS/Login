cdcd5bda8b9e4c4cf34d8bf9d6229d98f6387e06
jagan <jagan1419@gmail.com>
2015-06-01 12:56:17 +0530

    commit via netbeans; project creation

--- Login/src/java/controller/LoginInf.java
+++ Login/src/java/controller/LoginInf.java
@@ -20,73 +20,34 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInf extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            String id = request.getParameter("name");
+        String id = request.getParameter("name");
 		String pass = request.getParameter("password");
 		response.setContentType("index.html");
-        
-           
-                RequestDispatcher rd = null;
-                request.setAttribute(id, "name");
-                if(id.equals("name") && pass.equals("pass")){
-                    rd = request.getRequestDispatcher("welocome.html");
-                    rd.forward(request, response);
-                }
-                else{
-                    out.println("<b>Invalid Login Info.</b><br>");
-                    rd = request.getRequestDispatcher("error.html");
-                    rd.include(request, response);
-                }        }
-    }
+        try (PrintWriter out = response.getWriter()) {
+            RequestDispatcher rd = null;
+            request.setAttribute(id, "name");
+            if(id.equals("name") && pass.equals("pass")){
+                rd = request.getRequestDispatcher("welcome.html");
+                rd.forward(request, response);
+            }
+            else{
+                
+                rd = request.getRequestDispatcher("error.html");
+                rd.include(request, response);
+            }
+        }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+	}
 }
+
+
+
+        
